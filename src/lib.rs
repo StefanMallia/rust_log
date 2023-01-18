7abcc071d58b7d77f437d371b5922cbd92278a5c
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2023-01-18 23:06:53 +0100

    Logging setup for rust

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub extern crate log;
+pub use log::*;
+
+pub fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
